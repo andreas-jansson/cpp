978757b74075e052a871bb77c288ebe87bf8d8b4
andreas <andreas.jansson@protonmail.com>
2020-02-18 22:22:51 +0100

    Initial Commit

---
+++ testplusplus/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  testplusplus
+//
+//  Created by Andréas on 2020-02-18.
+//  Copyright © 2020 A. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
